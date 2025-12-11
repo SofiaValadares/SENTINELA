c8375a455d9cce9e6495347424ea50c0f862cf57
SofiaValadares <svc3@cesar.school>
2025-12-10 21:20:12 -0300

    cores

--- fast_sentinela/fast_sentinela/app.py
+++ fast_sentinela/fast_sentinela/app.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .enum.RoutesEnum import RoutesEnum
 from .controller.PredictController import router as predict_router
 from .controller.ImageController import router as image_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 
 @app.get(RoutesEnum.PING.value)
 def read_root():
